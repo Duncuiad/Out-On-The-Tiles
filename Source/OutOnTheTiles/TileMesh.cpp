0a5bb52fde8f5df4c322cce0e4e27b97a555b2dc
Duncuiad <36989446+Duncuiad@users.noreply.github.com>
2021-04-23 11:22:58 +0200

    Started working on TileMesh

--- Source/OutOnTheTiles/TileMesh.cpp
+++ Source/OutOnTheTiles/TileMesh.cpp
@@ -3,3 +3,5 @@
 
 #include "TileMesh.h"
 
+// Initialization of the inflation constant's value
+const float UTileMesh::INFLATION_CONSTANT = sqrt(2.f + sqrt(3.f));
--- Source/OutOnTheTiles/TileMesh.h
+++ Source/OutOnTheTiles/TileMesh.h
@@ -2,6 +2,10 @@
 
 #pragma once
 
+#include "Vertex.h"
+#include "HalfEdge.h"
+#include "Face.h"
+
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
 #include "TileMesh.generated.h"
@@ -13,5 +17,21 @@ UCLASS()
 class OUTONTHETILES_API UTileMesh : public UObject
 {
 	GENERATED_BODY()
+
+	// Maximum number of subdivisions to allow (including the root)
+	const static int MAX_SUBDIVISION_DEPTH = 8;
+
+	// The list of vertices in the mesh
+
+	// The list of half-edges in the mesh
+
+	// The list of faces in the mesh, except for tiles (see below)
+
+	// The list of tiles in the mesh, i. e. the maximally subdivided faces.
+
+public:
+
+	// This is the value that lengths contract by at each subsequent subdivision
+	const static float INFLATION_CONSTANT; // sqrt(2 + sqrt(3))
 	
 };
